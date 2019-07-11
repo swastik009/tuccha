5ba1e672f2a6478d7624f1ede6476130fbe311c9
swastik009 <swastik.thapaliya@gmail.com>
2019-07-11 22:59:30 +0545

    Services files moved to Repositories folder

---
+++ app/Http/Repositories/SocialAccountRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Swastik
+ * Date: 7/11/2019
+ * Time: 10:54 PM
+ */
+
+namespace App\Http\Repositories;
+
+
+class SocialAccountRepository
+{
+
+}
---
+++ app/Http/Repositories/SocialUserResolver.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Swastik
+ * Date: 7/11/2019
+ * Time: 10:56 PM
+ */
+
+namespace App\Http\Repositories;
+
+
+class SocialUserResolver
+{
+
+}
